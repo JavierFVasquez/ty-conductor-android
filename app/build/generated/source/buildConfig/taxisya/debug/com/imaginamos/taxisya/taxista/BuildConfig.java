0fc7702fa1c29d3bbd78666a9bf9482eb372782f
sammyceluvans <sammy.cuartas@celuvans.com.co>
2017-03-14 14:51:58 -0500

    first commit

---
+++ app/build/generated/source/buildConfig/taxisya/debug/com/imaginamos/taxisya/taxista/BuildConfig.java
@@ -0,0 +1,19 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.imaginamos.taxisya.taxista;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "co.taxisya.conductor";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "taxisya";
+  public static final int VERSION_CODE = 3;
+  public static final String VERSION_NAME = "1.2";
+  // Fields from build type: debug
+  public static final String HOST = "www.taxisya.co";
+  public static final boolean USE_CRASHLYTICS = true;
+  public static final boolean USE_PUSH = true;
+  // Fields from product flavor: taxisya
+  public static final String HOST_NODE = "http://www.taxisya.co:3701/";
+}
